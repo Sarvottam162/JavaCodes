742bb629e02b177888c342bdca5a57e0d06898d6
Sarvottam162 <147574477+Sarvottam162@users.noreply.github.com>
2025-02-18 19:52:53 +0530

    sum of a and b
    completed

--- abc.java
+++ abc.java
@@ -1,3 +1,9 @@
 class abc{
-    
+    public static void sum(int a , int b){
+        int sum = a+b ;
+        System.out.println(sum);
+    }
+    public static void main(String[] args) {
+        sum(4, 5);
+    }
 }
