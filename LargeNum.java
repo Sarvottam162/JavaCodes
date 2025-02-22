d914bb74b4c472c0c3e0d0a52f71e213a77ea938
Sarvottam162 <147574477+Sarvottam162@users.noreply.github.com>
2025-02-22 07:28:24 +0530

    largest num in array
    added to find smallest also

--- LargeNum.java
+++ LargeNum.java
@@ -2,18 +2,22 @@ public class LargeNum {
     
     public static int getLargest(int numbers[]){
         int largest=Integer.MIN_VALUE;
-     for(int i =0; i<numbers.length; i++){
-        if(numbers[i]>largest){
-            largest=numbers[i];
-        }
-     }
+        int smallest = Integer.MAX_VALUE;
+            for(int i =0; i<numbers.length; i++){
+
+                if(numbers[i]>largest){
+                    largest=numbers[i];
+                }
+                if (numbers[i]<smallest){
+                    smallest=numbers[i];
+                }
+            }
+    System.out.println("smallest number = "+smallest);
    return largest;
     }
     public static void main(String args[]) {
-       int numbers[]={2,4,6,8,15};
-       
-      int Maximumvalue=getLargest(numbers);
-
+       int numbers[]={2,4,6,8,50};
+       int Maximumvalue=getLargest(numbers);
         System.out.println("the maximum value present in the given array is :"+Maximumvalue);
     }
 }
