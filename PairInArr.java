abb3e1774757543b5ee90d614f4668ddfd7dc34b
Sarvottam162 <147574477+Sarvottam162@users.noreply.github.com>
2025-02-23 07:58:47 +0530

    Pair Of Array
    Print All The Pair Of Array

---
+++ PairInArr.java
@@ -0,0 +1,23 @@
+public class PairInArr {
+
+public static void printpair(int numbers[]){
+    int totalpair=0;
+        for (int i =0; i<numbers.length; i++){
+
+                int curr=numbers[i];
+
+            for(int j =i+1; j<numbers.length; j++){
+
+                System.out.print("("+curr+","+ numbers[j]+") ");
+                totalpair++;
+            }
+            System.out.println();
+        }
+        System.out.println("Total pair is "+totalpair);
+}
+
+    public static void main(String[] args) {
+        int numbers[]={2,4,6,8,10,12,14};
+        printpair(numbers);
+    }
+}
